009b946b56a8da34b546c96e274a9d2f063ee703
emmieIO <markonuoha6@gmail.com>
2023-06-15 07:44:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function uses the puts function to print out
+ * the specified string with a newline.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+/* The puts function prints out the string with a newline */
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
