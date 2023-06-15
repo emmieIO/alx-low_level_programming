b4a21ce1fcba09cc72e0c5f180a8d730f1f1130b
emmieIO <markonuoha6@gmail.com>
2023-06-15 07:51:34 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function uses the puts function to print out
+ * the specified string with a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* The puts function prints out the string with a newline */
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
