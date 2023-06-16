246561424338a522b5140954710c86127f1064f0
emmieIO <markonuoha6@gmail.com>
2023-06-16 06:56:46 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,15 +21,15 @@ lastdigit = n % 10;
 
 if (lastdigit > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, lastdigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 }
 else if (lastdigit == 0)
 {
-printf("Last digit of %d is %d and is 0", n, lastdigit);
+printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 }
 else if (lastdigit < 6 && lastdigit != 0)
 {
-printf("Last digit of %d is %d and is less than 6 not 0", n, lastdigit);
+printf("Last digit of %d is %d and is less than 6 not 0\n", n, lastdigit);
 }
 return (0);
 }
