eaed863346b34d6a7a8b7e193603e95f5f63d709
emmieIO <markonuoha6@gmail.com>
2023-06-16 09:32:16 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,30 +6,23 @@
  * Description: Prints all possible combinations of two two-digit numbers
  *              in ascending order, with each number printed with two digits.
  *
- * Return: (0) Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-int num;
+int num1, num2;
 
-for (num = 0; num < 100; num++)
+for (num1 = 0; num1 <= 99; num1++)
 {
-putchar((num / 10) + '0');
-putchar((num % 10) + '0');
-putchar(' ');
-
-if (num != 99)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
-putchar((num / 10) + '0');
-putchar((num % 10) + 1 + '0');
-putchar(',');
-
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
 putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
 
-putchar((num / 10) + 1 + '0');
-putchar((num % 10) + '0');
-
-if (num != 98)
+if (num1 != 98 || num2 != 99)
 {
 putchar(',');
 putchar(' ');
