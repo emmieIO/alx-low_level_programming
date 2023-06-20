f2e2cb8096412aacd9bacee06ad6ba50b4488d3a
emmieIO <markonuoha6@gmail.com>
2023-06-20 10:19:01 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,22 +1,42 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * times_table - Generates a multiplication time table from 0 to 9.
+ * times_table - Generates a times time table from 0 to 9.
  *
- * Return: None (void).
+ * Description:
+ *     This function generates a times table from 0 to 9
+ *     and uses the write() function to print the table.
  */
 void times_table(void)
 {
+int table[10][10];
 int i, j;
+char buffer[5]; /* Buffer to store the formatted number */
 
-for (i = 0; i <= 9; i++)
+/* Generate the times table */
+for (i = 0; i < 10; i++)
 {
-for (j = 0; j <= 9; j++)
+for (j = 0; j < 10; j++)
 {
-int result = i * j;
-_putchar(result);
+table[i][j] = i * j;
 }
-_putchar("\n");
+}
+
+/* Print the times table */
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+/* Format the number and store it in the buffer */
+snprintf(buffer, sizeof(buffer), "%d ", table[i][j]);
+
+/* Write the formatted number to the standard output */
+write(STDOUT_FILENO, buffer, sizeof(buffer) - 1);
+}
+
+/* Write a newline character to the standard output */
+write(STDOUT_FILENO, "\n", 1);
 }
 }
 
