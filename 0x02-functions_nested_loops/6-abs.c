05bb73a7e50cbf34e9603d3cdfb2e3aee6783c88
emmieIO <markonuoha6@gmail.com>
2023-06-20 09:07:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * custom_abs - Computes the absolute value of a number.
+ * @n: The number for which to compute the absolute value.
+ *
+ * Return: The absolute value of the given number.
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
