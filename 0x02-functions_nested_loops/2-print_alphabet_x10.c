ce2ef84766b12f221d4c6e7c3539684b3a682128
emmieIO <markonuoha6@gmail.com>
2023-06-20 07:30:31 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function.
+ *
+ * Description: function to print alphabets 10 times.
+ */
+
+void print_alphabet_x10(void)
+{
+	int counter = 0;
+
+	for (counter; counter < 10; counter++)
+	{
+		print_alphabet();
+	}
+}
