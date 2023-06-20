bc87bcfad2a98a17398bed0f76f265557a00060a
emmieIO <markonuoha6@gmail.com>
2023-06-20 10:08:25 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - Generates a multiplication time table from 0 to 9.
+ *
+ * Return: None (void).
+ */
+void times_table(void)
+{
+int i, j;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+int result = i * j;
+_putchar(result + '0');
+}
+_putchar("\n");
+}
+}
+
