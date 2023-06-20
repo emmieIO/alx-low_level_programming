8e39ba8dcb0314300c966f9ce7ea1777ce62957c
emmieIO <markonuoha6@gmail.com>
2023-06-20 07:53:37 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,14 @@ void print_alphabet_x10(void)
 {
 	int counter = 0;
 	char c;
-	for (counter; counter < 10; counter++)
+
+	while (counter < 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-			
 		}
-		_putchar('\n')
+		_putchar('\n');
+		counter++;
 	}
 }
