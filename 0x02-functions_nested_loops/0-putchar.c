bdddcc71f5784cb6df0e2390ca1785c040638284
emmieIO <markonuoha6@gmail.com>
2023-06-20 04:25:43 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Program entry point.
+ *
+ * Description: A program that print __putchar.
+ *
+ * Return: Always return (0) Success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
