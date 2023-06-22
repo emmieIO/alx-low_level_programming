a2bb5c478055dfab5a9bb5a1065f3da49345799d
emmieIO <markonuoha6@gmail.com>
2023-06-22 07:44:10 -0700

    7-task solved

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_diagonal - Print a diagonal line with spaces and backslashes
+ * @n: Number of times to print the diagonal line
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n <= 0)
+{
+putchar('\n');
+}
+else
+{
+for (i = 1; i <= n; i++)
+{
+for (j = 0; j <= i; j++)
+{
+putchar(' ');
+}
+putchar('\\');
+putchar('\n');
+}
+}
+}
