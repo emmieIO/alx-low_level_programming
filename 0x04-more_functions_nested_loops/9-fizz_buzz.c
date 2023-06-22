ff89bd34861039d01e5ffd2b3405b7682d0d4cec
emmieIO <markonuoha6@gmail.com>
2023-06-22 09:01:07 -0700

    8-task solved

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,13 @@
  *
  * Return: Always 0 (indicating success)
  */
-
-
 int main(void)
 {
 int i;
-for (i = 0; i <= 100; i++)
-{
-if(i == 0)
-{
-i++;
-}
 
-else if (i % 3 == 0 && i % 5 == 0)
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz ");
 }
@@ -42,7 +36,5 @@ printf("%d ", i);
 }
 
 printf("\n");
-
 return (0);
 }
-
