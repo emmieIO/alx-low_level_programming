250e142a5e100cc41e287456dcc0ce5a681e7233
emmieIO <markonuoha6@gmail.com>
2023-06-22 09:58:08 -0700

    9-task solved

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - Prints an inverse triangle
+ * shape with spaces before '#' symbols.
+ * @size: The size of the triangle.
+ */
+void print_triangle(int size)
+{
+int i, j;
+
+if (size <= 0)
+{
+putchar('\n');
+return;
+}
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+putchar(' ');
+}
+
+for (j = 0; j <= i; j++)
+{
+putchar('#');
+}
+
+putchar('\n');
+}
+}
