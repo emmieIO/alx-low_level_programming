d5da86b44b31234b6cdfc70bb2443ceb74bfafc6
emmieIO <markonuoha6@gmail.com>
2023-06-22 01:39:30 -0700

    date-debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,55 +9,29 @@
 * @year: year
 * Return: void
 */
+
 void print_remaining_days(int month, int day, int year)
 {
-int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+    {
+        if (month >= 2 && day >= 60)
+        {
+            day++;
+        }
 
-if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
-{
-days_in_month[2] = 29; // Update February to have 29 days in a leap year
+        printf("Day of the year: %d\n", day);
+        printf("Remaining days: %d\n", 366 - day);
+    }
+    else
+    {
+        if (month == 2 && day == 60)
+        {
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
+        }
+    }
 }
-
-if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-}
-else
-{
-int remaining_days = 0;
-for (int i = month + 1; i <= 12; i++)
-{
-remaining_days += days_in_month[i];
-}
-remaining_days += days_in_month[month] - day;
-
-printf("Day of the year: %d\n", get_day_of_year(month, day, year));
-printf("Remaining days: %d\n", remaining_days);
-}
-}
-
-/**
-* get_day_of_year - calculates the day of the year for a given date
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: day of the year
-*/
-int get_day_of_year(int month, int day, int year)
-{
-int day_of_year = day;
-int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
-{
-days_in_month[2] = 29; // Update February to have 29 days in a leap year
-}
-
-for (int i = 1; i < month; i++)
-{
-day_of_year += days_in_month[i];
-}
-
-return day_of_year;
-}
-
