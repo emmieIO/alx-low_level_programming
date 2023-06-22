a79a776fbf3723e13a9ad44c99bce4fa5a677db2
emmieIO <markonuoha6@gmail.com>
2023-06-22 01:32:20 -0700

    date-debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,21 +11,24 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 {
-if (month >= 2 && day >= 60)
+if (month > 2 || (month == 2 && day == 29))
 {
-day++;
-}
-
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+printf("Day of the year: %d\n", day + 1);
+printf("Remaining days: %d\n", 366 - (day + 1));
 }
 else
 {
-if (month == 2 && day == 60)
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
+}
+}
+else
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+if (month == 2 && day == 29)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 }
 else
 {
@@ -34,3 +37,4 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
+
