60e05dd1df5e16534c1b42da1e0035a8d45a1109
emmieIO <markonuoha6@gmail.com>
2023-06-22 05:25:26 -0700

    1-task solved

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is a decimal digit
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is a decimal digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (isdigit(c))
+{
+return (1);
+}
+return (0);
+}
+
