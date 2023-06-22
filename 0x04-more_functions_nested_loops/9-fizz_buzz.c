de16cd1c1e93e4ec0e72f97621b2e9e0253f6ad8
emmieIO <markonuoha6@gmail.com>
2023-06-22 08:29:02 -0700

    8-task solved

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Description: Implements the FizzBuzz algorithm,
+ * which prints numbers from 1 to 100.
+ * For multiples of 3, it prints "Fizz".
+ * For multiples of 5, it prints "Buzz".
+ * For numbers that are multiples of both 3 and 5, it prints "FizzBuzz".
+ * Prints the number itself for all other cases.
+ *
+ * Return: Always 0 (indicating success)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("fizzBuzz ");
+}
+else
+{
+if (i % 3 == 0)
+{
+printf("fizz ");
+}
+else
+{
+if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+
+}
+
+
+}
+}
+return (0);
+}
