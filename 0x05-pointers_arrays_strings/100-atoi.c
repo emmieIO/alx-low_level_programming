ea0c6b390a252d863ed977afa1be03c91bafdfc6
emmieIO <markonuoha6@gmail.com>
2023-06-26 08:56:52 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,21 +12,37 @@ int sign = 1;
 int result = 0;
 int i = 0;
 
+if (s[0] == '-')
+{
+sign = -1;
+i++;
+}
+
 while (s[i] != '\0')
 {
-if (s[i] == '-')
-sign *= -1;
-else if (s[i] >= '0' && s[i] <= '9')
+if (s[i] >= '0' && s[i] <= '9')
 {
-result = result * 10 + (s[i] - '0');
+int digit = s[i] - '0';
 
-/* Break if the next character is not a digit */
-if (s[i + 1] < '0' || s[i + 1] > '9')
+
+if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
+{
+if (sign == 1)
+return INT_MAX;
+else
+return INT_MIN;
+}
+
+result = result * 10 + digit;
+}
+else
+{
+
 break;
 }
 
 i++;
 }
 
-return ((sign * result));
+return (sign * result);
 }
