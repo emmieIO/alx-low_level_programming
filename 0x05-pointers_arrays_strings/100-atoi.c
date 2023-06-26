fecfa1660e486a2c998d6d70a59433967fd205ff
emmieIO <markonuoha6@gmail.com>
2023-06-26 09:12:42 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - Convert a string to an integer.
@@ -17,18 +18,16 @@ while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
 i++;
 
 /* Check for sign */
-if (s[i] == '-' || s[i] == '+')
-{
+if (s[i] == '-' || s[i] == '+') {
 sign = (s[i] == '-') ? -1 : 1;
 i++;
 }
 
 /* Convert digits to integer */
-while (s[i] >= '0' && s[i] <= '9')
-{
+while (s[i] >= '0' && s[i] <= '9') {
 /* Check for overflow */
 if (result > (INT_MAX - (s[i] - '0')) / 10)
-return (0); /* Overflow occurred, return 0 */
+return 0; /* Overflow occurred, return 0 */
 
 result = result * 10 + (s[i] - '0');
 i++;
