642cc6c40216df26457bd591658f2e8ad5019b75
emmieIO <markonuoha6@gmail.com>
2023-06-26 02:35:26 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ * swap_int - function to be executed
+ *
+ * @a: first int params
+ * @b: second int params
+ *
+ */
+void swap_int(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
