42e08035ce89ea55528482dcd5fa2d34bddf8374
emmieIO <markonuoha6@gmail.com>
2023-06-29 04:46:20 -0700

    102-magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+* main - Entry point of the program.
+* and prints the updated value of a[2].
+* Betty is here to ensure
+* a smooth execution and adherence to coding standards.
+* So sit back, relax, and let Betty guide you through this program!
+*
+* Return: Always 0 (indicating successful execution)
+*/
 int main(void)
 {
 int n;
@@ -8,17 +17,8 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-*(p + 5) = 98;
-/* ...so that this prints 98\n */
+
+*(p + 7) = 98;
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
-
