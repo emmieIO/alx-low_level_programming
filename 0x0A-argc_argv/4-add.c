7dcf376cc486ce6573118eff0c80ac473f8fd0f9
emmieIO <markonuoha6@gmail.com>
2023-07-07 18:59:32 +0100

    4th task done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,28 +5,30 @@
  * main - Program entry point
  *
  * @argc: comand count
- * @argc: command array
+ * @argv: command array
  *
  * Return: returns success
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	int count = 0;
-	int result = 0;
+int count = 0;
+int result = 0;
 
-	while (argv[count]) {
-		int value = atoi(argv[count]);
+while (argv[count])
+{
+int value = atoi(argv[count]);
 
-		if (!isdigit(argv[count][0])) {
-			printf("Error\n");
-			return 1;
-		} else {
-			result += value;
-		}
-
-		count++;
-	}
-
-	printf("%d\n", result);
-	return 0;
+if (!isdigit(argv[count][0]))
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+result += value;
+}
+count++;
+}
+printf("%d\n", result);
+return (0);
 }
