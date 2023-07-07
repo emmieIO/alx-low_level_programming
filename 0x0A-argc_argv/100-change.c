0d7a2aca69aa540ac41da2451f00603eead6c1ed
emmieIO <markonuoha6@gmail.com>
2023-07-07 19:34:13 +0100

    4th task done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -54,15 +54,17 @@ int minimumCoins(int cents)
  */
 int main(int argc, char *argv[])
 {
+	int cents, result;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-	int result = minimumCoins(cents);
+	result = minimumCoins(cents);
 
 	printf("%d\n", result);
 
