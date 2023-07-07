406434a45af2ec79bece5c011d56def968130d58
emmieIO <markonuoha6@gmail.com>
2023-07-07 15:59:01 +0100

    Task 3 done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Program Entry Point
+ * @argc: Keeps count
+ * @argv: arrays of command
+ *
+ * Return: Always return success(0)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int count = 0;
+
+	while(argv[count])
+	{
+		printf("%s\n",argv[count]);
+		count++;
+	}
+
+	return (0);
+
+}
