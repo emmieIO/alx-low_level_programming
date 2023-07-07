8de6f4c0e9713d853442ed47e9ed12db718cd412
emmieIO <markonuoha6@gmail.com>
2023-07-07 15:37:03 +0100

    Task 2 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * @argc: keep count of the command line
+ * @argv: you have your own function
+ * Return: always returns (0)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
