f49a86c314c62820848c02e2ccb382110e03b3b4
emmieIO <markonuoha6@gmail.com>
2023-07-10 15:27:14 +0100

    task 0-done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	array = (char *)malloc(size * sizeof(array));
+	array = malloc(size * sizeof(array));
 
 	if (size == 0 || array == NULL)
 	{
