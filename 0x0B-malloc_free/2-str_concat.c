3ef87298177ab3ca05787ce04307fd7082c8e440
emmieIO <markonuoha6@gmail.com>
2023-07-10 16:06:41 +0100

    task 2-done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,24 +10,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 	size_t total_len;
 	size_t len1;
 	size_t len2;
 	char *concatenated_str;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	total_len = len1 + len2;
-	
-
 	concatenated_str = (char *)malloc((total_len + 1) * sizeof(char));
 
 	if (concatenated_str == NULL)
+	{
 		return NULL;
+	}
 
 	strcpy(concatenated_str, s1);
 	strcat(concatenated_str, s2);
