05d62c32cee61583aa772452bb01eef2e06f5bb8
emmieIO <markonuoha6@gmail.com>
2023-07-31 22:28:51 +0000

    Completion of task 2

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,25 @@
 #include "lists.h"
+
+/**
+ * listint_len - a function that prints the length of alist.
+ *
+ * @h: pointer to node.
+ *
+ * Return: The number of elements in the linked list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+
+		h = h->next;
+		count++;
+
+	}
+
+	return (count);
+
+}
