4ee349988129c401b10721bf88bf525bcb20f3a2
emmieIO <markonuoha6@gmail.com>
2023-07-31 23:20:46 +0000

    Completion of task 3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *newNode = (struct listint_t *) malloc(sizeof(listint_t));
+	listint_t *newNode = malloc(sizeof(listint_t));
 
 	if (newNode == NULL)
 	{
