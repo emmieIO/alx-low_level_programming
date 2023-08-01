bb059402505814684d203c56338e05b2b40bc3b7
emmieIO <markonuoha6@gmail.com>
2023-08-01 21:51:54 +0000

    re-commiting task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,68 +1,31 @@
 #include "lists.h"
-size_t loop_length(const listint_t *head);
-
 /**
- * print_listint_safe - Function to print a listint_t linked list safely
- * @head: Pointer to the head node of the list
- * Return: The number of nodes in the list
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: Pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *slow = head;
+	const listint_t *fast = head;
 	size_t node_count = 0;
-	size_t loop_len;
-
-	if (head == NULL)
-	{
-		return (0);
-	}
-
-	loop_len = loop_length(head);
-
-	while (head != NULL)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		node_count++;
-
-		if (node_count == loop_len)
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
-		}
-
-	}
-
-	return (node_count);
-
-}
-
-
-/**
- * loop_length - Function to calculate the length of a loop in a list
- * @head: Pointer to the head node of the list
- * Return: The length of the loop if there is a loop, 0 otherwise
- */
-size_t loop_length(const listint_t *head)
-{
-	const listint_t *slow = head, *fast = head;
-	size_t count = 0;
 
 	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		node_count++;
+
 		slow = slow->next;
 		fast = fast->next->next;
 
 		if (slow == fast)
 		{
-			slow = slow->next;
-			count = 1;
-			while (slow != fast)
-			{
-				slow = slow->next;
-				count++;
-			}
-			return (count);
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
+			exit(98);/* Exit with status 98 if a loop is detected */
 		}
 	}
-	return (0);
+
+	return (node_count);
 }
+
