9f922ef58db3d8cc999794af7f9a78244a214f00
emmieIO <markonuoha6@gmail.com>
2023-08-01 20:09:32 +0000

    commiting task 100

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * reverse_listint - Entry point to function
+ * @head: Pointer to node
+ *
+ * Return: returns a struct typedef
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *current, *prev, *next;
+	
+	current = head;
+	prev = NULL;
+
+	if(head == NULL)
+	{
+		return NULL;
+	}
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	head = prev;
+	return (head);
+
+}
