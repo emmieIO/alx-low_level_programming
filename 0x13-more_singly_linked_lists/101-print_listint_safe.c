5f178c6d57d4e06ba44d91f88be420c633be6912
emmieIO <markonuoha6@gmail.com>
2023-08-01 22:58:23 +0000

    re-commiting task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,31 +1,42 @@
 #include "lists.h"
+
 /**
- * print_listint_safe - Prints a listint_t linked list.
+ * print_listint_safe - Prints a listint_t linked list and detects cycles.
  * @head: Pointer to the head of the linked list.
  *
  * Return: The number of nodes in the list.
+ *
+ * Description: This function prints the elements of a listint_t linked list.
+ * If the linked list contains a cycle, the program is terminated with status 98.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head;
-	const listint_t *fast = head;
-	size_t node_count = 0;
+const listint_t *slow = head;
+const listint_t *fast = head;
+size_t node_count = 0;
 
-	while (slow != NULL && fast != NULL && fast->next != NULL)
-	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		node_count++;
+while (slow != NULL && fast != NULL && fast->next != NULL)
+{
+printf("[%p] %d\n", (void *)slow, slow->n);
+node_count++;
 
-		slow = slow->next;
-		fast = fast->next->next;
+slow = slow->next;
+fast = fast->next->next;
 
-		if (slow == fast)
-		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			exit(98);/* Exit with status 98 if a loop is detected */
-		}
-	}
+if (slow == fast)
+{
+printf("-> [%p] %d\n", (void *)slow, slow->n);
+exit(98);
+}
+}
 
-	return (node_count);
+while (head != NULL)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+node_count++;
+}
+
+return (node_count);
 }
 
