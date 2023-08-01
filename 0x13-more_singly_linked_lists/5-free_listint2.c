094586ef85d9a3f2c846b7f6f7bd2c63a4604847
emmieIO <markonuoha6@gmail.com>
2023-08-01 19:11:02 +0000

    re-commiting task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,11 @@ void free_listint2(listint_t **head)
 {
 
 	listint_t *temp;
-
+	
+	if(head == NULL)
+	{
+		return;
+	}
 
 	while (*head != NULL)
 	{
@@ -16,5 +20,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = temp;
 	}
-	*head = NULL;
+
 }
