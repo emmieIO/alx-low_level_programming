7881bb287a6e5fc81bf5af12e63c3de7e05418f4
emmieIO <markonuoha6@gmail.com>
2023-08-01 12:05:35 +0000

    commiting task ten

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
- * sum_listin - Entry point of sum function
+ * sum_listint - Entry point of sum function
  * @head: Params of pointer to node
+ *
  * Return: returns int of sum node data;
  */
 
@@ -10,6 +11,7 @@ int sum_listint(listint_t *head)
 {
 	listint_t *temp;
 	int sum;
+
 	temp = head;
 	sum = 0;
 
@@ -21,7 +23,6 @@ int sum_listint(listint_t *head)
 	while (temp != NULL)
 	{
 		sum += temp->n;
-		
 		temp = temp->next;
 	}
 
