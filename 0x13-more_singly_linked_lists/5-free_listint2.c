378312928583a40c6391878f48b8b3b5c5b79dd6
emmieIO <markonuoha6@gmail.com>
2023-08-01 02:16:48 +0000

    final solution for task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function that free memomry allocated to a node in list
+ * @head: paremeter of poitner to node
+ */
+
+void free_listint2(listint_t **head)
+{
+        listint_t *temp;
+	
+	
+        while (*head != NULL)
+        {	
+                temp = *head;
+                *head = (*head)->next;
+                free(temp);
+        }
+
+	
+
+}
+
