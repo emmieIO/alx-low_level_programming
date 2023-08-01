b077ec2065e1c4f0cc061fc33e2a866c4b48f40a
emmieIO <markonuoha6@gmail.com>
2023-08-01 02:43:10 +0000

    final solution for task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,17 +9,19 @@ void free_listint2(listint_t **head)
 {
 
 	listint_t *temp;
+	listint_t *current;
 
 	if (*head == NULL)
 	{
 		return;
 	}
 
+	current = *head;
 
-	while (*head)
+	while (current)
 	{
-		temp = *head;
-		*head = (*head)->next;
+		temp = current;
+		current = current->next;
 		free(temp);
 	}
 
