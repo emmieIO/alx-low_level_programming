6486f57a824b3c62217d1bba41ee019a5902e0fb
emmieIO <markonuoha6@gmail.com>
2023-08-03 19:41:31 +0000

    re-commiting Task-7

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * get_endianness - Checks the endianness of the system.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+	unsigned int test = 1;
+	unsigned char *byte_pointer = (unsigned char *)&test;
+
+	return (*byte_pointer) ? 1 : 0;
+}
