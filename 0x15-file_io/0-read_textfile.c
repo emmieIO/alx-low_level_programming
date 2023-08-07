e172fa462a3cd30902080ad1ff533be97e1eb826
emmieIO <markonuoha6@gmail.com>
2023-08-07 14:57:50 +0000

    Task One correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,25 +6,29 @@
 /**
  * read_textfile - entry point of read file program
  * @letters: letters is the number of letters it should read and print
- *
+ * @filename: filname
  * Return: return an unsigned int of the number of letters
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char buf[1024];
-    ssize_t fd, t, w;
+	char buf[1024];
+	ssize_t fd, t, w;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
 
-    t = read(fd, buf, letters);
-    w = write(STDOUT_FILENO, buf, t);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    close(fd);
-    return (w);
+	close(fd);
+	return (w);
 }
