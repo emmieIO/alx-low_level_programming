cf85b7de6aca071538eaac6b4886ef2397106e02
emmieIO <markonuoha6@gmail.com>
2023-08-07 14:53:48 +0000

    first commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * read_textfile - entry point of read file program
+ * @letters: letters is the number of letters it should read and print
+ *
+ * Return: return an unsigned int of the number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    char buf[1024];
+    ssize_t fd, t, w;
+
+    if (filename == NULL)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    t = read(fd, buf, letters);
+    w = write(STDOUT_FILENO, buf, t);
+
+    close(fd);
+    return (w);
+}
