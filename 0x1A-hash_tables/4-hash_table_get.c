5acaa82eb229357b4815feedbd5f2c5723ad7a8f
emmieIO <markonuoha6@gmail.com>
2023-10-05 22:30:46 +0000

    re-runnung commit

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_get - Retrieves the value associated with a key.
  * @ht: The hash table to look into.
@@ -9,23 +8,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *current;
+	hash_node_t *node;
+	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
-        	return (NULL);
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 
-	/* Traverse the linked list at the specified index */
-	current = ht->array[index];
-	while (current != NULL)
-	{
-		if (strcmp(current->key, key) == 0)
-			/* Found the key, return the associated value */
-			return (current->value);
-			current = current->next;
-	}
-	/* Key not found */
-	return (NULL);
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
 }
