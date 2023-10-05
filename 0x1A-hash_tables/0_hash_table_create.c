4d76303810b8afb5d30fa52a20c7e5b2e8175e91
emmieIO <markonuoha6@gmail.com>
2023-10-05 22:17:32 +0000

    re-runnung commit

--- 0x1A-hash_tables/0_hash_table_create.c
+++ 0x1A-hash_tables/0_hash_table_create.c
@@ -15,23 +15,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size < 1)
 		return (NULL);
 
-	/* Allocate memory for the hash table structure */
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
-	/* Allocate memory for the array of pointers to hash nodes */
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
-		/* If allocation fails, free the hash table structure */
 		free(new_table);
 		return (NULL);
 	}
-	/* Set the size of the hash table */
 	new_table->size = size;
-
-	/* Initialize each element of the array to NULL */
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
 
